67f94f33424bb7c2b5e13539bb798a2258eb3a60
Siim Türk <siimtuerk@gmail.com>
2022-09-08 16:27:53 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from "axios";
+import VueAxios from "vue-axios";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/src/jquery.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+Vue.use(VueAxios,axios)
 
 Vue.config.productionTip = false
 
