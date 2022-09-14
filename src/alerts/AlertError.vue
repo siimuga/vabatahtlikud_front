c4e56baf065f3fb01fa5d4faf6c872ea082c56d2
ttruus <tarvotruus@gmail.com>
2022-09-14 15:35:37 +0300

    TARVO TASK1 login and register

---
+++ src/alerts/AlertError.vue
@@ -0,0 +1,13 @@
+<template>
+  <div v-if="errorMessage.length > 0" class="alert alert-danger" role="alert">
+    {{errorMessage}}
+  </div>
+</template>
+<script>
+export default {
+  name: 'AlertError',
+  props: {
+    errorMessage: ''
+  }
+}
+</script>
---
+++ src/alerts/AlertSuccess.vue
@@ -0,0 +1,13 @@
+<template>
+  <div v-if="successMessage.length > 0" class="alert alert-success" role="alert">
+    {{ successMessage }}
+  </div>
+</template>
+<script>
+export default {
+  name: 'AlertSuccess',
+  props: {
+    successMessage: ''
+  }
+}
+</script>
