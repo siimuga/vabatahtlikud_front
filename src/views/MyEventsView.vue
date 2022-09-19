66fc3d4f3aa3b0df63736c32c63b82a9a97d14b9
Siim Türk <siimtuerk@gmail.com>
2022-09-19 16:00:58 +0300

    DEVELOP-FRONT SIIMT TASK1 my event page (display current and passed event)

--- src/views/MyEventsView.vue
+++ src/views/MyEventsView.vue
@@ -22,63 +22,70 @@
     <div>
       <div class="col-sm">
         <h3 align="left" style="margin: 5px"><span style="color: #2c3e50">Eesolevad üritused</span></h3>
+        <div v-if="activeEvents.length>0">
+          <table class="table table-hover">
+            <thead>
+            <tr>
+              <th scope="col"></th>
+              <th scope="col">Ürituse nimi</th>
+              <th scope="col">Vabatahtlike arv</th>
+              <th scope="col">Registreerunute arv</th>
+              <th scope="col">Roll</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="event in activeEvents">
+              <th scope="row">{{ event.seqNr }}</th>
+              <td>{{ event.eventName }}</td>
+              <td>{{ event.volunteersRequired }}</td>
+              <td>{{ event.volunteersAttended }}</td>
+              <td>{{ event.roleName }}</td>
+              <td v-if="event.roleName === 'vabatahtlik'" v-model="event.eventId">
+                <button type="button" style="margin: 5px" class="btn btn-danger" v-on:click="cancelParticipation">Tühista
+                  osalemine
+                </button>
+              </td>
+              <td v-if="event.roleName === 'korraldaja'">
+                <button type="button" style="margin: 5px" class="btn btn-outline-dark" v-on:click="toChangeEvent">
+                  Muuda
+                </button>
+                <button type="button" style="margin: 5px" class="btn btn-danger" v-on:click="toDeleteEvent">Kustuta
+                </button>
+              </td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
 
-        <table class="table table-hover">
-          <thead>
-          <tr>
-            <th scope="col"></th>
-            <th scope="col">Ürituse nimi</th>
-            <th scope="col">Roll</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>sündmus</td>
-            <td>korraldaja</td>
-            <td>
-              <button type="button" style="margin: 5px" class="btn btn-outline-dark" v-on:click="toChangeEvent">
-                Muuda
-              </button>
-              <button type="button" style="margin: 5px" class="btn btn-danger" v-on:click="toDeleteEvent">Kustuta
-              </button>
-            </td>
-
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>sündmus</td>
-            <td>vabatahtlik</td>
-            <td>
-              <button type="button" style="margin: 5px" class="btn btn-danger" v-on:click="toDeleteEvent">Tühista
-                osalemine
-              </button>
-            </td>
-          </tr>
-          </tbody>
-        </table>
+        <h3 align="center" style="margin: 5px"><span style="color: #2c3e50" v-if="activeEvents.length===0">Eesolevaid üritusi pole</span>
+        </h3>
         <br>
         <h3 align="left" style="margin: 5px"><span style="color: #2c3e50">Möödunud üritused</span></h3>
-        <table class="table table-hover">
-          <thead>
-          <tr>
-            <th scope="col"></th>
-            <th scope="col">Ürituse nimi</th>
-            <th scope="col">Roll</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>sündmus</td>
-            <td>korraldaja</td>
-          </tr>
-
-          </tbody>
-        </table>
-
+        <div v-if="pastEvents.length>0">
+          <table class="table table-hover">
+            <thead>
+            <tr>
+              <th scope="col"></th>
+              <th scope="col">Ürituse nimi</th>
+              <th scope="col">Vabatahtlike arv</th>
+              <th scope="col">Registreerunute arv</th>
+              <th scope="col">Roll</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="event in pastEvents">
+              <th scope="row">{{ event.id }}</th>
+              <td>{{ event.eventName }}</td>
+              <td>{{ event.volunteersRequired }}</td>
+              <td>{{ event.volunteersAttended }}</td>
+              <td>{{ event.roleName }}</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+        <h3 align="center" style="margin: 5px"><span style="color: #2c3e50" v-if="pastEvents.length===0">Möödunuid üritusi pole</span>
+        </h3>
       </div>
-
     </div>
   </div>
 
@@ -86,14 +93,45 @@
 </template>
 
 <script>
+import AlertSuccess from "@/alerts/AlertSuccess";
+
 export default {
   name: "AccountView",
+  components: {AlertSuccess},
   props: {},
 
   data: function () {
     return {
       divToHomePage: true,
-      example: 'Mari'
+      example: 'Mari',
+      userId: sessionStorage.getItem('userId'),
+      selectedEventId : 0,
+      pastEvents: [
+        {
+          id: 0,
+          roleName: '',
+          eventName: '',
+          volunteersRequired: 0,
+          volunteersAttended: 0
+        }
+      ],
+      activeEvents: [
+        {
+          seqNr: 0,
+          eventId: 0,
+          roleName: '',
+          eventName: '',
+          volunteersRequired: 0,
+          volunteersAttended: 0
+        }
+      ],
+      volunteerDeleteRequest: [
+        {
+          userId: sessionStorage.getItem('userId'),
+          eventId: event.eventId
+        }
+      ],
+      successMessage: ''
     }
   },
 
@@ -110,6 +148,48 @@ export default {
     toDeleteEvent: function () {
       this.$router.push({name: 'homeRoute'})
     },
+    cancelParticipation: function () {
+        this.$http.delete("/event/event/cancel", this.volunteerDeleteRequest
+        ).then(response => {
+          alert(this.successMessage = 'Osalemine tühistatud')
+          location.reload();
+          console.log(response.data)
+        }).catch(error => {
+          console.log(error)
+        })
+    },
+    findPastEvents: function () {
+      this.$http.get("/event/event/user/history", {
+        params: {
+          userId: this.userId
+        }
+      })
+          .then(response => {
+            this.pastEvents = response.data
+            console.log(response.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+    findAllActiveEvents: function () {
+      this.$http.get("/event/event/user", {
+        params: {
+          userId: this.userId
+        }
+      })
+          .then(response => {
+            this.activeEvents = response.data
+            console.log(response.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+  },
+  mounted() {
+    this.findPastEvents()
+    this.findAllActiveEvents()
   }
 }
 </script>
