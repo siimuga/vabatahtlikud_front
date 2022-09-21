4592bbfa273ebbb471f317d1da94fa7c7a949bd6
Siim Türk <siimtuerk@gmail.com>
2022-09-21 13:35:11 +0300

    DEVELOP-FRONT SIIMT TASK6 show events part2 WIP

--- src/views/AllEventsView.vue
+++ src/views/AllEventsView.vue
@@ -3,14 +3,13 @@
     <div class="container-xxl">
       <div class="row">
         <div class="col-xl">
-
           <button type="button" style="margin: 5px" class="btn btn-outline-dark" v-on:click="toHomePage">Esilehele
           </button>
-
           <div class="btn-group" style="margin: 5px">
             <select class="btn btn-outline-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown"
                     aria-expanded="false" v-model="selectedCategory">
               <option disabled value="">Vali valdkond</option>
+              <option value="kõik"> Kõik valdkonnad</option>
               <option v-for="option in categoryList" :value="option">{{ option.name }}</option>
             </select>
           </div>
@@ -18,7 +17,7 @@
             <select class="btn btn-outline-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown"
                     aria-expanded="false" v-model="selectedCounty">
               <option disabled value="">Vali maakond</option>
-              <option :value="0"> kõik</option>
+              <option value="kõik"> Kõik maakonnad</option>
               <option v-for="option in countyList" :value="option">{{ option.name }}</option>
             </select>
           </div>
@@ -37,52 +36,75 @@
     </div>
     <br>
     <br>
+
     <div class="container-xxl" style="alignment: center">
-      <div class="row">
-        <div class="col-md-3">
-          <div type="button" class="content" v-on:click="toEventPage"><a href="#">
-            <div class="content-overlay"></div>
-            <img type="button" class="content-image" src="../assets/EventView/event_image.jpg">
+      <div class="row row-cols-4">
+        <div class="col-md-3" v-for="event in eventsList" v-if="selectedCategory==='kõik' && selectedCounty==='kõik'" v-model="findAllEvents">
+          <h3 class="content-title">{{ event.eventName }}</h3>
+          <h3 class="content-title">{{ event.volunteersAttended }}/{{ event.volunteersRequired }}</h3>
+          <div type="button" class="content" v-on:click="toEventPage(event)"><a href="#">
+            <div class="content-overlay">
+            </div>
+            <div v-if="event.hasPicture = false">
+              <img type="button" class="content-image" src="../assets/EventView/event_image.jpg">
+            </div>
+            <div v-if="event.hasPicture = true">
+              <img type="button" class="content-image" src="../assets/EventView/event_image.jpg">
+            </div>
             <div class="content-details fadeIn-bottom">
-              <h3 class="content-title">Sündmuse nimi</h3>
               <p class="content-text"><i class="fa fa-map-marker"></i>Loe edasi</p>
             </div>
           </a></div>
-        </div>
-        <div class="col-md-3">
-          <div type="button" class="content" v-on:click="toEventPage"><a href="#">
-            <div class="content-overlay"></div>
-            <img class="content-image" src="../assets/EventView/event_image.jpg">
-            <div class="content-details fadeIn-bottom">
-              <h3 class="content-title">Sündmuse nimi</h3>
-              <p class="content-text"><i class="fa fa-map-marker"></i>Loe edasi</p>
-            </div>
-          </a></div>
-        </div>
-        <div class="col-md-3">
-          <div type="button" class="content" v-on:click="toEventPage"><a href="#">
-            <div class="content-overlay"></div>
-            <img class="content-image" src="../assets/EventView/event_image.jpg">
-            <div class="content-details fadeIn-bottom">
-              <h3 class="content-title">Sündmuse nimi</h3>
-              <p class="content-text"><i class="fa fa-map-marker"></i>Loe edasi</p>
-            </div>
-          </a></div>
-        </div>
-        <div class="col-md-3">
-          <div type="button" class="content" v-on:click="toEventPage"><a href="#">
-            <div class="content-overlay"></div>
-            <img class="content-image" src="../assets/EventView/event_image.jpg">
-            <div class="content-details fadeIn-bottom">
-              <h3 class="content-title">Sündmuse nimi</h3>
-              <p class="content-text"><i class="fa fa-map-marker"></i>Loe edasi</p>
-            </div>
-          </a></div>
+          <br>
         </div>
       </div>
     </div>
 
+    <div class="container-xxl" style="alignment: center">
+      <div class="row row-cols-4">
+        <div class="col-md-3" v-for="event in eventsListCategory" v-if="selectedCategory!=='kõik' && selectedCounty==='kõik'"> <!--v-model="findByCategory(selectedCategory)"-->
+          <h3 class="content-title">{{ event.eventName }}</h3>
+          <h3 class="content-title">{{ event.volunteersAttended }}/{{ event.volunteersRequired }}</h3>
+          <div type="button" class="content" v-on:click="toEventPage(event)"><a href="#">
+            <div class="content-overlay">
+            </div>
+            <div v-if="event.hasPicture = false">
+              <img type="button" class="content-image" src="../assets/EventView/event_image.jpg">
+            </div>
+            <div v-if="event.hasPicture = true">
+              <img type="button" class="content-image" src="../assets/EventView/event_image.jpg">
+            </div>
+            <div class="content-details fadeIn-bottom">
+              <p class="content-text"><i class="fa fa-map-marker"></i>Loe edasi</p>
+            </div>
+          </a></div>
+          <br>
+        </div>
+      </div>
+    </div>
 
+    <div class="container-xxl" style="alignment: center">
+      <div class="row row-cols-4">
+        <div class="col-md-3" v-for="event in eventsListCounty" v-if="selectedCategory==='kõik' && selectedCounty!=='kõik'"><!-- v-model="findByCounty(selectedCounty)"-->
+          <h3 class="content-title">{{ event.eventName }}</h3>
+          <h3 class="content-title">{{ event.volunteersAttended }}/{{ event.volunteersRequired }}</h3>
+          <div type="button" class="content" v-on:click="toEventPage(event)"><a href="#">
+            <div class="content-overlay">
+            </div>
+            <div v-if="event.hasPicture = false">
+              <img type="button" class="content-image" src="../assets/EventView/event_image.jpg">
+            </div>
+            <div v-if="event.hasPicture = true">
+              <img type="button" class="content-image" src="../assets/EventView/event_image.jpg">
+            </div>
+            <div class="content-details fadeIn-bottom">
+              <p class="content-text"><i class="fa fa-map-marker"></i>Loe edasi</p>
+            </div>
+          </a></div>
+          <br>
+        </div>
+      </div>
+    </div>
   </div>
 
 </template>
@@ -96,6 +118,9 @@ export default {
       countyList: [],
       categoryList: [],
       eventsList: [],
+      eventsListCategory: [],
+      eventsListCounty: [],
+      eventsListCategoryAndCounty: [],
       selectedCategory: '',
       selectedCounty: ''
     }
@@ -104,6 +129,7 @@ export default {
   methods: {
     toHomePage: function () {
       this.$router.push({name: 'homePageRoute'})
+      location.reload()
     },
     toLogInPage: function () {
       this.$router.push({name: 'logInRoute'})
@@ -129,11 +155,48 @@ export default {
             console.log(response.data)
           })
     },
+    findByCounty: function (selectedCounty) {
+      this.$http.get("/event/events/county")
+          .then(response => {
+            this.eventsListCounty = response.data
+            console.log(response.data)
+          }).catch(error => {
+        console.log(error)
+      })
+    },
+
+    findByCategory: function () {
+      this.$http.get("/event/events/category")
+          .then(response => {
+            this.eventsListCategory = response.data
+            console.log(response.data)
+          }).catch(error => {
+        console.log(error)
+      })
+    },
+
+    findEventsByCategoryAndCounty: function () {
+      this.$http.get("/event/event")
+          .then(response => {
+            this.eventsListCategoryAndCounty = response.data
+            console.log(response.data)
+          }).catch(error => {
+        console.log(error)
+      })
+    },
+    toEventPage: function (event) {
+      sessionStorage.setItem('eventId', event.eventId)
+      this.$router.push({name: 'eventRoute'})
+    }
+
   },
   mounted() {
     this.findAllCategories()
     this.findAllCounties()
     this.findAllEvents()
+    sessionStorage.removeItem('eventId')
+    this.selectedCounty = 'kõik'
+    this.selectedCategory = 'kõik'
   }
 }
 </script>
